e72ebc6e42e6ff76a6faa6585c0c09691e79dd7e
asus <yohana.aprila@si.ukdw.ac.id>
2020-10-01 06:50:54 +0700

    Hello world

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variabel
+        final TextView txtView = (TextView)findViewById(R.id.mainActivityTextView);
+        Button myBtn = (Button)findViewById(R.id.Button);
+        final EditText myEditText = (EditText)findViewById(R.id.editText1);
+        Button btnHelp = (Button)findViewById(R.id.btnHelp);
+
+        //action
+        txtView.setText(R.string.text_hello_world);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+               // Log.d("COBA KLIKK ZEYENG", myEditText.getText().toString());
+                txtView.setText(myEditText.getText().toString());
+            }
+        });
+    btnHelp.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent( packageContext: MainActivity.this,helpActivity.class);;
+            Bundle b = new Bundle();
+
+            b.putString("help_string",myEditText.getText().toString());
+            intent.putExtras(b);
+
+            startActivity(intent);
+        }
+    });
+
+
     }
+
 }
